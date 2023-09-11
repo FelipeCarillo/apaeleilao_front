b284a57a6d29c2783b47215cc416e23206a4c4f9
Gabriel Merola <gabrielmerola12@gmail.com>
2023-09-11 11:45:54 -0300

    Página de Login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Login from './pages/Login'
 import Principal from './pages/Principal'
+import Cadastro from './pages/Cadastro'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Principal />}/>
         <Route path='/login' element={<Login />}/>
+        <Route path='/cadastro' element={<Cadastro />}/>
       </Routes>
     </Router>
   )
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,51 @@
+import {Link} from 'react-router-dom'
+
 export default function Login() {
     const imgLogo = 'https://apaeleilaoimt.s3.sa-east-1.amazonaws.com/galery/logo-apaeleilao-branco.jpg'
     
     return (
         <>
-        <header className='bg-blue-700 p-4 rounded-b-2xl flex justify-center'>
+        <header className='bg-blue-600 p-4 rounded-b-2xl flex justify-center'>
             <img src={imgLogo} alt='logo da APAE leilão' className='w-[200px] md:w-[250px]'/>
         </header>
-        <main>
-            
+        <main className="px-4">
+            <h1 className="text-4xl text-center mt-4 font-semibold text-blue-900 md:text-5xl">Login</h1>
+            <div className="flex justify-center">
+                <div className="w-[90%] h-[2px] bg-gray-200 my-2 md:my-4" />
+            </div>
+
+            {/* FORMULARIO DE LOGIN */}
+            <form>
+                <div className="flex flex-col text-xl gap-2 mb-4 md:items-center">
+                    <label htmlFor="login">Email ou CPF:</label>
+                    <input className="bg-gray-200 rounded-full py-1 px-3 md:w-1/2" type="text" name="login" id="login"/>
+                </div>
+
+                <div className="flex flex-col text-xl gap-2 mb-4 md:items-center">
+                    <label htmlFor="password">Senha:</label>
+                    <input className="bg-gray-200 rounded-full py-1 px-3 md:w-1/2" type="password" name="password" id="password"/>
+                </div>
+
+                <div className="flex justify-center mt-8">
+                    <div className="w-[90%] h-[2px] bg-gray-200" />
+                </div>
+
+                <div className="flex flex-col items-center gap-4 my-4">
+                    <Link to="/">Esqueceu sua Senha?</Link>
+                    <div className='flex items-center gap-1'>
+                        <input className="w-4 h-4" type="checkbox" id="conected" name="conected"/>
+                        <label htmlFor="conected">Deseja continuar conectado?</label>
+                    </div>
+                </div>
+                
+                <div className="flex justify-center mt-4">
+                    <button className="bg-yellow-300 py-4 px-12 rounded-full">LOGIN</button>
+                </div>
+            </form>
         </main>
+        <footer className='fixed bottom-0 w-full text-center font-semibold py-4 bg-blue-600'>
+            <p>Ainda não possui uma conta? <Link className='text-yellow-400' to="/cadastro">Clique Aqui</Link> </p>
+        </footer>
         </>
     )
 }
