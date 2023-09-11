0d62d57e0e5dbae280cbdd77ae0967ca3fb60005
Gabriel Merola <gabrielmerola12@gmail.com>
2023-09-11 12:49:11 -0300

    Ajuste na tela de Login

--- src/pages/Cadastro.js
+++ src/pages/Cadastro.js
@@ -5,7 +5,9 @@ export default function Cadastro() {
         <>
         <header></header>
         <main></main>
-        <footer></footer>
+        <footer className='fixed bottom-0 w-full text-center font-semibold py-4 bg-blue-600 shadow-xl'>
+            <p className='text-white'>Já possui uma conta? <Link className='text-yellow-400' to="/cadastro">Clique Aqui</Link> </p>
+        </footer>
         </>
     )
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,11 +5,11 @@ export default function Login() {
     
     return (
         <>
-        <header className='bg-blue-600 p-4 rounded-b-2xl flex justify-center shadow-xl'>
-            <img src={imgLogo} alt='logo da APAE leilão' className='w-[200px] md:w-[250px]'/>
+        <header className='bg-blue-600 flex justify-center p-4 rounded-b-2xl shadow-xl'>
+            <img src={imgLogo} alt='logo da APAE leilão' className='w-[200px] self-center md:w-[250px]'/>
         </header>
-        <main className="px-4">
-            <h1 className="text-4xl text-center mt-4 font-semibold text-blue-900 md:text-5xl">Login</h1>
+        <main className="px-4 flex flex-col justify-center">
+            <h1 className="text-4xl text-center mt-4 font-medium text-blue-900 md:text-5xl">Login</h1>
             <div className="flex justify-center">
                 <div className="w-[90%] h-[2px] bg-gray-200 my-2 md:my-4" />
             </div>
@@ -24,27 +24,27 @@ export default function Login() {
                 <div className="flex flex-col text-xl gap-2 mb-4 md:items-center">
                     <label className='md:w-1/2' htmlFor="password">Senha:</label>
                     <input className="bg-gray-200 rounded-full py-1 px-3 md:w-1/2" type="password" name="password" id="password"/>
+                    <Link className='underline max-md:self-start md:w-1/2' to="/">Esqueceu sua Senha?</Link>
                 </div>
 
                 <div className="flex justify-center mt-8">
                     <div className="w-[90%] h-[2px] bg-gray-200" />
                 </div>
 
-                <div className="flex flex-col items-center gap-4 my-4">
-                    <Link className='underline' to="/">Esqueceu sua Senha?</Link>
-                    <div className='flex items-center gap-1'>
+                <div className="flex flex-col items-center text-xl gap-4 my-4">
+                    <div className='flex items-center max-md:self-start gap-1 md:w-1/2'>
                         <input className="w-4 h-4" type="checkbox" id="conected" name="conected"/>
                         <label htmlFor="conected">Deseja continuar conectado?</label>
                     </div>
                 </div>
                 
                 <div className="flex justify-center mt-4">
-                    <button className="bg-yellow-300 py-4 px-12 rounded-full">LOGIN</button>
+                    <button className="bg-yellow-300 py-4 px-16 text-xl rounded-full">LOGIN</button>
                 </div>
             </form>
         </main>
-        <footer className='fixed bottom-0 w-full text-center font-semibold py-4 bg-blue-600 shadow-xl'>
-            <p>Ainda não possui uma conta? <Link className='text-white' to="/cadastro">Clique Aqui</Link> </p>
+        <footer className='fixed bottom-0 mt-8 w-full text-center font-medium py-4 bg-blue-600 shadow-xl'>
+            <p className='text-white'>Ainda não possui uma conta? <Link className='text-yellow-400' to="/cadastro">Clique Aqui</Link> </p>
         </footer>
         </>
     )
