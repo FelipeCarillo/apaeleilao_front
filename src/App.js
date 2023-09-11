7bc3e398f93c28e183846de13efa09bf720d1ab9
Gabriel Merola <gabrielmerola12@gmail.com>
2023-09-11 10:55:00 -0300

    Configuração do react-Router

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
-import axios from 'axios';
-import './App.css';
-import { useState } from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Login from './pages/Login'
+import Principal from './pages/Principal'
 
-function App() {
-  const [email, setEmail] = useState(null)
-  const [passwd, setPasswd] = useState(null)
-  
-  function apiGetUser() {
-    const apiUrl = "https://nad4eryio2anscac6c5ru3yeoi0qdyct.lambda-url.sa-east-1.on.aws"
-    
-    const requestData = {
-      "email": email,
-      "password": passwd,
-    };
-    axios.post(apiUrl, requestData).then(function (response) {
-      // Manipule a resposta aqui
-      console.log('Resposta do servidor:', response.data);
-    })
-    .catch(function (error) {
-      // Manipule erros aqui
-      console.error('Erro na requisição:', error);
-    });
-  }
-
-
+export default function App() {
   return (
-    <main>
-      <label htmlFor='email'>email:</label>
-      <input type='text' name='email' id='email'  onChange={(e) => {setEmail(e.target.value)}}/>
-
-      <label htmlFor='passwd'>Senha:</label>
-      <input type='password' name='passwd' id='passwd' onChange={(e) => {setPasswd(e.target.value)}} />
-
-      <button onClick={apiGetUser}>Enviar</button>
-    </main>
-  );
+    <Router>
+      <Routes>
+        <Route path='/' element={<Principal />}/>
+        <Route path='/login' element={<Login />}/>
+      </Routes>
+    </Router>
+  )
 }
-
-export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+export default function Login() {
+    const imgLogo = 'https://apaeleilaoimt.s3.sa-east-1.amazonaws.com/galery/logo-apaeleilao-branco.jpg'
+    
+    return (
+        <>
+        <header className='bg-blue-700 p-4 rounded-b-2xl flex justify-center'>
+            <img src={imgLogo} alt='logo da APAE leilão' className='w-[200px] md:w-[250px]'/>
+        </header>
+        <main>
+            
+        </main>
+        </>
+    )
+}
