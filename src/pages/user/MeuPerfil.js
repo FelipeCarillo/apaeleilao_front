a33c57ba1329b223fd7d06e7bb6106d23219bf2e
Guilherme Rodriguez <guilhermeseabrarodriguez@gmail.com>
2023-10-23 07:44:27 -0300

    Interface Meu Perfil

--- src/pages/user/MeuPerfil.js
+++ src/pages/user/MeuPerfil.js
@@ -4,6 +4,7 @@ import Navbar from "../../components/Navbar";
 export default function MeuPerfil() {
     return (
         <>
+        {/* NAVBAR */}
         <Navbar pag='Perfil'/>
 
         <main>
