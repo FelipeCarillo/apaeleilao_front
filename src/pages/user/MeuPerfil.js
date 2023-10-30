780b44d77f19d4fdd71fc7b81778ef7a02598b88
Gabriel Merola <gabrielmerola12@gmail.com>
2023-10-30 09:06:39 -0300

    getUser na interface meuPerfil

--- src/pages/user/MeuPerfil.js
+++ src/pages/user/MeuPerfil.js
@@ -2,60 +2,97 @@ import { useState } from "react";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 import ReactInputMask from "react-input-mask";
+import { ToastContainer, toast } from 'react-toastify';
 
 export default function MeuPerfil() {
+    const [inicials, setInicials] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
+    const [password, setPassword] = useState('')
+
     const [process,setProcess] = useState(false)
     const [viewPass,setviewPass] =useState(false)
 
     const [placeholder, setPlaceholder] = useState(false)
+
+    function getUser() {
+        fetch('https://aoltolsszk.execute-api.sa-east-1.amazonaws.com/prod/apae-leilao/get-user', {
+            method: 'GET',
+            headers: {
+                'Authorization': localStorage.getItem('token'),
+            }
+        }).then(response => {
+            if(response.ok){
+                return response.json()
+            }else{
+                return Promise.reject(response);
+            }
+        }).then(data => {
+            setInicials(data.body.first_name.charAt(0) + data.body.last_name.charAt(0))
+            setFirstName(data.body.first_name)
+            setEmail(data.body.email)
+            setPhone(data.body.phone)
+            setPassword(data.body.password)
+        }).catch(error => {
+            // AQUI VC CONTROLA O RESULTADO (STATUS CODE + MESSAGE)
+            console.log("ERROOOO " + error.status);
+            // 3. get error messages, if any
+            error.json().then((json: any) => {
+                console.log(json);
+                toast.error(json.message, {
+                    position: "top-center",
+                    autoClose: 3000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                })
+            })
+        })
+    }
+
     return (
         <>
         {/* NAVBAR */}
         <Navbar pag='Perfil'/>
 
         <main>
+            {getUser()}
+            <ToastContainer position="top-center" autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light" />
             {/* BOTOES DO TOPO */}
             <div className="mx-2 mt-2 grid justify-items-end">
                 <button className="border bg-[#f4db33] text-2xl rounded-full py-1 px-2"><i className="fa-solid fa-headset"></i></button>
             </div>
-            {/* <div>
-            <button className="border bg-[#2c4fbc] text-2xl py-1 px-3"><i class="fa-solid fa-person"></i></button>
-            </div> */}
             <section className="border-b border-black mx-[100px]">
                 <div className="mb-24 mt-4 text-center">
-                    <label className="border-2 border-black bg-[#d9d9d9] text-6xl rounded-full py-7 p-6">BG</label>
+                    <label className="border-2 border-black bg-[#d9d9d9] text-6xl rounded-full py-7 p-6">{inicials}</label>
                 </div>
             {/* DADOS */}
             </section>
            <form className="mx-[100px] text-2xl mr-24 mt-4 ml-24 box-content h-100 w-2200">
-                {/* <div className="flex justify-end">
-                    <button className="border bg-[#f4db33] text-xl rounded-full py-1 px-2"><i className="fa-solid fa-pen"></i></button>
-                </div> */}
                 <div className="flex flex-col gap-2 mb-4">
                     <label className='md:w-1/2' htmlFor="Perfil">Nome:</label>
-                    <input type="text" className="bg-gray-300 rounded-full py-1 px-3 w-full"/>
+                    <input type="text" className="bg-gray-300 rounded-full py-1 px-3 w-full" disabled value={firstName}/>
                 </div>
                 <div className="flex flex-col gap-2 mb-4 ">
                     <label className="block">
                         <span className='block'htmlFor="Perfil">Email</span>
-                            <input type="email" className="w-full peer border-2 bg-gray-300 rounded-full py-1 px-3" placeholder={placeholder ? "exemplo@gmail.com" : ""} onBlur={() => {setPlaceholder(false)}} onFocus={() => {setPlaceholder(true)}}/>
+                            <input type="email" className="w-full peer border-2 bg-gray-300 rounded-full py-1 px-3" placeholder={placeholder ? "exemplo@gmail.com" : ""} onBlur={() => {setPlaceholder(false)}} onFocus={() => {setPlaceholder(true)}} value={email} disabled/>
                                 <p className="hidden peer-invalid:block text-red-600 text-base">
                                      Insira um email valido!
                                 </p>
                     </label>
-                    {/* <label className='md:w-1/2' htmlFor="Perfil">Email:</label>
-                    <input type="email" className="border-2 border-black"/>
-                    <p class="mt-2 invisible peer-invalid:visible text-red-600 text-sm">
-                        Informe um email valido!
-                    </p> */}
                 </div>
                 <div className="flex flex-col gap-2 mb-4">
                     <label className='md:w-1/2' htmlFor="Perfil">Celular:</label>
-                    <ReactInputMask className="flex bg-gray-300 rounded-full py-1 px-3 w-full" mask="(99)99999-9999" name="tel" id="tel"/>
+                    <ReactInputMask className="flex bg-gray-300 rounded-full py-1 px-3 w-full" mask="(99)99999-9999" name="tel" id="tel" value={phone} disabled/>
                 </div>
                 <div className="flex flex-col mb-4">
                     <label className='md:w-1/2' htmlFor="Perfil">Senha:</label>
-                        <input type={`${viewPass ?'text': 'password'}`} className="bg-gray-300 rounded-full py-1 px-3 w-full"/>
+                        <input type={`${viewPass ?'text': 'password'}`} className="bg-gray-300 rounded-full py-1 px-3 w-full" value={password} disabled/>
                     <div className="flex items-center justify-between">
                         <label className="text-lg underline underline-offset-2 mb-2 flex justify-items-start cursor-pointer">Redefinir Senha</label>
                         <label className={`text-lg flex items-end cursor-pointer mb-2`}onClick={()=>{setviewPass(!viewPass)}}>Mostrar Senha</label>
@@ -67,14 +104,6 @@ export default function MeuPerfil() {
            <div className="flex justify-center">
                 <button className="bg-yellow-300 py-4 px-16 text-xl mb-4 rounded-full cursor-pointer flex items-center gap-2" onClick={()=>{setProcess(!process)}}><i className={`fa-solid fa-pen ${process ? '' : ''}`}></i>Editar</button>
             </div>
-           {/* <section>
-            <div className="flex flex-col gap-2 mb-4 mt-4 text-center">
-                <label className="text-5xl "htmlFor="Perfil">Feedback</label>
-                <h1 className="text-xl">Por favor, nos informe o que você está achando do site e sobre os leilões</h1>
-                <textarea className="border-2 border-black p-2 mx-80" placeholder="Digite aqui..."cols={25}></textarea>
-
-            </div>
-           </section> */}
         </main>
 
         <Footer />
