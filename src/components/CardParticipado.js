d94667e73a4a8b24c805ca8ab22c7a2a37060ecd
IsaiasCBLuz <isaiascbluz@gmail.com>
2023-11-27 19:25:28 -0300

    pagemento concluido

--- src/components/CardParticipado.js
+++ src/components/CardParticipado.js
@@ -1,22 +1,170 @@
+import { useEffect, useState} from "react";
+import Modal from 'react-modal';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export default function CardParticipados({data}){
+    const [images, setImages] = useState([]);
+    const [title, setTitle] = useState("Nome do Produto");
+    const [amount, setAmount] = useState("Valor do Produto");
+    const [auction_id, setAuction_id] = useState("");
+    const [end_date, setEnd_date] = useState("xx/xx/xx xx:xx");
+
+    const [payment, setPayment] = useState([]);
+    const [payment_code, setPayment_code] = useState("");
+
+    // const [imageNumber, setImageNumber] = useState(0);
+
+    const [modalIsOpen, setIsOpen] = useState(false);
+    function openModal(infos) {
+        // abre o modal
+        setIsOpen(true);
+
+        // seta as informações do modal
+        setImages(infos.images);
+        setTitle(infos.title);
+        setAmount(infos.amount);
+        setAuction_id(infos.auction_id);
+        setEnd_date(convertData(infos.end_date));
+
+    }
+    function closeModal() {
+      setIsOpen(false);
+    }
+    function convertData (data) {
+        var date = new Date(data*1000);
+        return date.toLocaleDateString();
+    }
+    function copiarCodigo () {
+        navigator.clipboard.writeText(payment_code);
+        toast.success('Código copiado com sucesso!', {
+            position: "top-center",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
+    }
+
+    // get payment
+    const fetchPaymentData = (auction_id) => {
+        fetch (process.env.REACT_APP_API + "/get-payment?auction_id=" + auction_id, {
+            method: "GET",
+            cors: "no-cors",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": localStorage.getItem("token"),
+            },
+        }).then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            setPayment(data.body);
+            setPayment_code((data.body.pix_code));
+        });
+    }
+    useEffect(() => {
+        if (modalIsOpen === true){
+            // Execute a função imediatamente
+            fetchPaymentData(auction_id);
+        
+            // Configurar um intervalo para executar a função a cada 5 segundos
+            const intervalId = setInterval(() => {
+            fetchPaymentData(auction_id);
+            }, 5000);
+        
+            // Limpar o intervalo quando o componente for desmontado
+            return () => clearInterval(intervalId);
+        }
+        else{
+            return;
+        }
+    }, [auction_id, modalIsOpen]);
+
     return (
         <>
         {data.map((data, index) => (
-            <div key={index} className="border-2 border-black rounded-[25px]">
             <div>
-                <img src={data.img} alt="Imagem do evento" className="w-[100%] h-[250px] rounded-t-[25px] object-cover"/>
+                {/* Card */}
+                <div key={index} className="border-2 border-black rounded-[25px]">
+                    <ToastContainer position="top-center" autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light" />
+                    <div>
+                        <img src={data.images[0].image_body} alt="Imagem do evento" className="w-[100%] h-[250px] rounded-t-[25px] object-cover"/>
+                    </div>
+                    <div className="text-center">
+                        <p className="text-2xl pb-0">{data.title}</p>
+                        <p className="text-2xl"><strong>Lance: R${data.amount}</strong></p>
+                        <p className="text-lg">Prazo: {convertData(data.end_date)}</p>
+                        { data.status_payment === 'PENDING' ? <button className="bg-yellow-300 p-2 border-2 border-black rounded-[45px] w-[80%] my-3 text-xl" onClick={() => {openModal(data)}}>Pagar</button>
+                        : data.status_payment === 'PAIED' ? <button className="bg-verde p-2 border-2 border-black rounded-[45px] w-[80%] my-3 text-white text-xl" disabled> <i className="fa-solid fa-check"></i> </button> 
+                        : <button className="bg-vermelho p-2 border-2 border-black rounded-[45px] w-[80%] my-3 text-white text-xl" disabled> <i className="fa-solid fa-x"></i> </button>}
+                    </div>
+                </div>
+                {/* Modal */}
+                <div>
+                  <Modal
+                    isOpen={modalIsOpen}
+                    ariaHideApp={false}
+                    onRequestClose={closeModal}
+                    style={
+                        {
+                            overlay:{
+                                backgroundColor: 'rgba(0,0,0,0.3)',
+                            },
+                            content:{
+                                backgroundColor: '#fff',
+                                borderRadius: '25px',
+                                width: '80%',
+                                height: window.innerWidth >= 768 ? '85%' : '75%',
+                                // height: '90%',
+                                margin: 'auto',
+                                padding: 0,
+                            }
+                        }
+                    }
+                    contentLabel="Example Modal"
+                  >
+                    {/* header */}
+                    <div className="flex justify-between bg-azul p-2 mb-5 md:mb-10">
+                        <p className="text-azul">a</p>
+                        <h2 className="text-white text-4xl">Pagamento</h2>
+                        <button className="text-4xl text-vermelho" onClick={closeModal}><i className="fa-solid fa-xmark"></i></button>
+                    </div>
+                    {/* Conteudo */}
+                    <div className="flex flex-col md:flex-row">
+                        {/* Lado Esquerdo - Card */}
+                        <div className="hidden md:flex md:flex-col w-[100%] md:w-[50%] md:border-r-2 md:border-black text-center">
+                            <h6 className="text-4xl underline mb-5">Produto</h6>
+                            <div key={index} className="border-2 border-black rounded-[25px] w-[90%] md:w-[60%] mx-auto">
+                                <div id="imageCard">
+                                    <img src={images.length > 0 ? images[0].image_body : 'https://placehold.co/500x500'} alt="Imagem do evento" className="w-[100%] h-[250px] rounded-t-[25px] object-cover"/>
+                                </div>
+                                <div className="text-center">
+                                    <p className="text-3xl pb-0 pt-3">{title}</p>
+                                    <p className="text-4xl py-10"><strong>Lance: R${amount}</strong></p>
+                                    <p className="text-xl pb-2">Prazo: {end_date}</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* Lado Direito - Pagamento */}
+                        <div className="w-[100%] md:w-[50%] px-10 text-center">
+                            <h6 className="text-4xl underline mb-5">PIX</h6>
+                            <img className="mx-auto border-2 border-gray-400 rounded-[10px] w-[100%] md:w-[50%]" src={payment.pix_base64 === undefined ? 'https://placehold.co/500x500' : `data:image/png;base64,${payment.pix_base64}`}></img>
+                            <button className="bg-yellow-300 mt-3 px-6 py-1 text-2xl rounded-[45px] border border-black" onClick={copiarCodigo}>Copiar Código</button>
+                            <p className="text-xl md:text-3xl mt-10">Status do Pagamento: <br/><strong>{data.status_payment === "PENDING" ? 'AGUARDANDO' : data.status_payment ? 'PAGO' : 'EXPIRADO'}</strong></p>
+                            <div className="mt-5">
+                                {data.status_payment === "PENDING" ? <div className="rounded-full mx-auto mt-1 text-6xl"><i className="fa-solid fa-spinner animate-spin"></i></div> 
+                                : data.status_payment === "PAIED" ? <div className="rounded-full mx-auto mt-1 text-6xl bg-green-500 w-[70px] h-[70px] flex items-center justify-center text-white animate-pulse"><i className="my-auto fa-solid fa-check"></i></div>
+                                : <div className="rounded-full mx-auto mt-1 text-6xl bg-red-600 w-[70px] h-[70px] flex items-center justify-center text-white animate-pulse"><i className="fa-solid fa-exclamation"></i></div>}   
+                            </div>
+                        </div>
+                    </div>
+                    
+                  </Modal>
+                </div>
             </div>
-            <div className="text-center">
-                <p className="text-2xl pb-0">{data.nome}</p>
-                <p className="text-lg pb-2">{data.categoria}</p>
-                <p className="text-2xl"><strong>Lance: {data.lance}</strong></p>
-                <p className="text-lg">Prazo: {data.prazo}</p>
-                {data.condicao === "Participou" ? <button className="bg-azul p-2 border-2 border-black rounded-[45px] w-[80%] my-3 text-white text-xl" disabled>Partipou</button> 
-                : data.condicao === 'Andamento' ? <button className="bg-yellow-300 p-2 border-2 border-black rounded-[45px] w-[80%] my-3 text-xl">Pagar</button>
-                : data.condicao === 'Pago' ? <button className="bg-verde p-2 border-2 border-black rounded-[45px] w-[80%] my-3 text-white text-xl" disabled> <i className="fa-solid fa-check"></i> </button> 
-                : <button className="bg-vermelho p-2 border-2 border-black rounded-[45px] w-[80%] my-3 text-white text-xl" disabled> <i className="fa-solid fa-x"></i> </button>}
-            </div>
-        </div>
         ))}
         </>
     )
